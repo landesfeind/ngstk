497d79ef26f33ce7501de8b0a680d2028b1aec89
Manuel Landesfeind <manuel@landesfeind.de>
2017-10-08 13:59:21 +0200

    Add input parameter for translate tool

--- src/tool/translate.rs
+++ src/tool/translate.rs
@@ -1,6 +1,5 @@
 extern crate clap;
 
-
 use io::fasta;
 use sequence::aminoacid::*;
 use sequence::dna::*;
@@ -16,19 +15,19 @@ pub struct Translate {}
 impl Tool for Translate {
     fn args<'a, 'b>(s: clap::App<'a, 'b>) -> clap::App<'a, 'b> {
         s.about("Translates a given input sequence into amino acid sequence")
-            .arg(
-                clap::Arg::with_name("sequence")
-                    .help(
-                        "The DNA sequence to translate. If '-', will read FASTA from stdin.",
-                    )
-            )
-            .arg(clap::Arg::with_name("output").long("out").short("o").help(
+            .arg(clap::Arg::with_name("sequence").help(
+                "The DNA sequence to translate. If '-', will read FASTA from stdin.",
+            ))
+            .arg(clap::Arg::with_name("in").long("in").short("i").takes_value(true).help(
+                "The input destination. If omitted or \"-\" will read from stdin",
+            ))
+            .arg(clap::Arg::with_name("out").long("out").short("o").takes_value(true).help(
                 "The output destination. If omitted or \"-\" will write to stdout",
             ))
     }
 
     fn run(matches: &clap::ArgMatches) {
-        match matches.value_of("output") {
+        match matches.value_of("out") {
             None => Translate::run_with_output(matches, stdout()),
             Some(target) => {
                 match target {
@@ -47,19 +46,39 @@ impl Tool for Translate {
 
 impl Translate {
     fn run_with_output<W: Write>(matches: &clap::ArgMatches, output: W) {
-
-        match matches.value_of("sequence") {
-            Some(seqstring) => {
-                match seqstring {
-                    "-" => Translate::translate_read_write(stdin(), output),
-                    _ => Translate::translate_string(&seqstring, output),
+        // Check for input sequence first
+        match matches.value_of("in") {
+            Some(filename) => {
+                debug!("Found parameter '--in' with value '{}'", filename);
+                match filename {
+                    "-" => {
+                        debug!("Reading from standard input");
+                        Translate::translate_read_write(stdin(), output);
+                    },
+                    _ => {
+                        debug!("Try to read from file: {}", filename);
+                        match File::open(filename) {
+                            Ok(fh) => Translate::translate_read_write(fh, output),
+                            Err(e) => panic!("Can not open '{}' for read: {}", filename, e),
+                        }
+                    }
                 }
             }
-            None => Translate::translate_read_write(stdin(), output),
+            // Check for additional command line parameters
+            None => {
+                match matches.value_of("sequence") {
+                    Some(seqstring) => {
+                        debug!("Found sequence with value {}", seqstring);
+                        match seqstring {
+                            "-" => Translate::translate_read_write(stdin(), output),
+                            _ => Translate::translate_string(&seqstring, output),
+                        }
+                    }
+                    None => Translate::translate_read_write(stdin(), output),
+                }
+            }
         }
     }
-
-
 
     fn translate_string<S: ToString, W: Write>(sequence: &S, mut output: W) {
         match DnaSequence::from_str(sequence.to_string().as_ref()) {
